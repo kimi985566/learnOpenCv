ca90e2ce3025358199b783386941238b940280d2
kimi985566 <chengyu_yang@outlook.com>
2018-02-28 16:09:38 +0800

    - Update

--- app/src/main/java/com/ycy/learnopencv/Bean/OpenCVConstants.java
+++ app/src/main/java/com/ycy/learnopencv/Bean/OpenCVConstants.java
@@ -7,86 +7,86 @@ package com.ycy.learnopencv.Bean;
 public interface OpenCVConstants {
 
     //灰度图形
-    public static String GRAY_TEST_NAME = "Image to gray";
-    public static String GREY_TEST_COM = "Gray Level Image";
+    String GRAY_TEST_NAME = "Image to gray";
+    String GREY_TEST_COM = "Gray Level Image";
 
     //Mat图像反色
-    public static String MAT_PIXEL_INVERT_NAME = "Convert:Mat";
-    public static String MAT_PIXEL_INVERT_COM = "Convert image in Mat";
+    String MAT_PIXEL_INVERT_NAME = "Convert:Mat";
+    String MAT_PIXEL_INVERT_COM = "Convert image in Mat";
 
     //Bitmap图像反色
-    public static String BITMAP_PIXEL_INVERT_NAME = "Convert:Bitmap";
-    public static String BITMAP_PIXEL_INVERT_COM = "Convert image in Bitmap";
+    String BITMAP_PIXEL_INVERT_NAME = "Convert:Bitmap";
+    String BITMAP_PIXEL_INVERT_COM = "Convert image in Bitmap";
 
     //调整亮度、对比度
-    public static String CONTRAST_RATIO_BRIGHTNESS_NAME = "Adjust CM&BR";
-    public static String CONTRAST_RATIO_BRIGHTNESS_COM = "Adjust contrast ratio and brightness";
+    String CONTRAST_RATIO_BRIGHTNESS_NAME = "Adjust CM&BR";
+    String CONTRAST_RATIO_BRIGHTNESS_COM = "Adjust contrast ratio and brightness";
 
     //创建mat空白图像
-    public static String IMAGE_CONTAINER_MAT_NAME = "Mat Operation";
-    public static String IMAGE_CONTAINER_MAT_COM = "Image Container: Mat, create a new image of mat";
+    String IMAGE_CONTAINER_MAT_NAME = "Mat Operation";
+    String IMAGE_CONTAINER_MAT_COM = "Image Container: Mat, create a new image of mat";
 
     //子图操作
-    public static String GET_ROI_NAME = "ROI Operation";
-    public static String GET_ROI_COM = "Get sub image";
+    String GET_ROI_NAME = "ROI Operation";
+    String GET_ROI_COM = "Get sub image";
 
     //均值滤波
-    public static String BOX_BLUR_IMAGE_NAME = "Box Blur";
-    public static String BOX_BLUR_IMAGE_COM = "Blur learning: Box Blur";
+    String BOX_BLUR_IMAGE_NAME = "Box Blur";
+    String BOX_BLUR_IMAGE_COM = "Blur learning: Box Blur";
 
     //高斯滤波
-    public static String GAUSSIAN_BLUR_IMAGE_NAME = "GAUSSIAN Blur";
-    public static String GAUSSIAN_BLUR_IMAGE_COM = "Blur learning: GAUSSIAN Blur";
+    String GAUSSIAN_BLUR_IMAGE_NAME = "GAUSSIAN Blur";
+    String GAUSSIAN_BLUR_IMAGE_COM = "Blur learning: GAUSSIAN Blur";
 
     //双边滤波
-    public static String BILATERAL_BLUR_IMAGE_NAME = "Bilateral Blur";
-    public static String BILATERAL_BLUR_IMAGE_COM = "Blur learning: Bilateral Blur";
+    String BILATERAL_BLUR_IMAGE_NAME = "Bilateral Blur";
+    String BILATERAL_BLUR_IMAGE_COM = "Blur learning: Bilateral Blur";
 
     //自定义模糊
-    public static String CUSTOM_BLUR_NAME = "Custom Blur";
-    public static String CUSTOM_BLUR_COM = "Blur Operation";
+    String CUSTOM_BLUR_NAME = "Custom Blur";
+    String CUSTOM_BLUR_COM = "Blur Operation";
 
     //自定义边缘提取
-    public static String CUSTOM_EDGE_NAME = "Custom Edge";
-    public static String CUSTOM_EDGE_COM = "Edge Operation";
+    String CUSTOM_EDGE_NAME = "Custom Edge";
+    String CUSTOM_EDGE_COM = "Edge Operation";
 
     //自定义锐化处理
-    public static String CUSTOM_SHARPEN_NAME = "Custom Sharpen";
-    public static String CUSTOM_SHARPEN_COM = "Sharpen Operation";
+    String CUSTOM_SHARPEN_NAME = "Custom Sharpen";
+    String CUSTOM_SHARPEN_COM = "Sharpen Operation";
 
     //腐蚀
-    public static String ERODE_NAME = "Erode";
-    public static String ERODE_COM = "Learning Erode";
+    String ERODE_NAME = "Erode";
+    String ERODE_COM = "Learning Erode";
 
     //膨胀
-    public static String DILATE_NAME = "Dilate";
-    public static String DILATE_COM = "Learning Dilate";
+    String DILATE_NAME = "Dilate";
+    String DILATE_COM = "Learning Dilate";
 
     //开操作
-    public static String OPEN_OPERATION_NAME = "Open Operation";
-    public static String OPEN_OPERATION_COM = "Learning Open Operation";
+    String OPEN_OPERATION_NAME = "Open Operation";
+    String OPEN_OPERATION_COM = "Learning Open Operation";
 
     //闭操作
-    public static String CLOSE_OPERATION_NAME = "Close Operation";
-    public static String CLOSE_OPERATION_COM = "Learning Close Operation";
+    String CLOSE_OPERATION_NAME = "Close Operation";
+    String CLOSE_OPERATION_COM = "Learning Close Operation";
 
     //形态学直线检测
-    public static String MORPH_LINE_OPERATION_NAME = "Line Detection";
-    public static String MORPH_LINE_OPERATION_COM = "形态学直线检测";
+    String MORPH_LINE_OPERATION_NAME = "Line Detection";
+    String MORPH_LINE_OPERATION_COM = "形态学直线检测";
 
     //阈值二值化
-    public static String THRESH_BINARY_NAME = "Thresh Binary";
-    public static String THRESH_BINARY_COM = "Thresh Binary Operation";
+    String THRESH_BINARY_NAME = "Thresh Binary";
+    String THRESH_BINARY_COM = "Thresh Binary Operation";
 
     //阈值反二值化
-    public static String THRESH_BINARY_INV_NAME = "Thresh Binary Inverse";
-    public static String THRESH_BINARY_INV_COM = "Thresh Binary Inverse Operation";
+    String THRESH_BINARY_INV_NAME = "Thresh Binary Inverse";
+    String THRESH_BINARY_INV_COM = "Thresh Binary Inverse Operation";
 
     //阈值截断
-    public static String THRESH_TRUNCAT_NAME = "Thresh Truncat";
-    public static String THRESH_TRUNCAT_COM = "Thresh Truncat Operation";
+    String THRESH_TRUNCAT_NAME = "Thresh Truncat";
+    String THRESH_TRUNCAT_COM = "Thresh Truncat Operation";
 
     //阈值取零
-    public static String THRESH_ZERO_NAME = "Thresh Zero";
-    public static String THRESH_ZERO_COM = "Thresh Zero Operation";
+    String THRESH_ZERO_NAME = "Thresh Zero";
+    String THRESH_ZERO_COM = "Thresh Zero Operation";
 }
